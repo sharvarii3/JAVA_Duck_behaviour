e4178ec8c2141d3f1b1b7deacb50b66734e5e992
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-03-08 23:56:42 +0530

    Add files via upload

---
+++ DecoyDuck.java
@@ -0,0 +1,13 @@
+public class DecoyDuck extends Duck {
+    
+    public DecoyDuck() {
+        flyBehaviour = new NotFly();
+        swimBehavior = new Float();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Decoy Duck.");
+    }
+}
+
