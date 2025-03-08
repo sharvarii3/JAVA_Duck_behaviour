d9860c318ece9b434aff9831e0f735c8a4b21e24
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-03-08 23:55:33 +0530

    Add files via upload

---
+++ RubberDuck.java
@@ -0,0 +1,12 @@
+public class RubberDuck extends Duck {
+    
+    public RubberDuck() {
+        flyBehaviour = new NotFly();
+        swimBehavior = new Float();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Rubber Duck.");
+    }
+}
