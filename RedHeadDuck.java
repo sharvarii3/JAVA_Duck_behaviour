743fd0e32116cd9a455382e869f4f31bb59d5977
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-03-08 23:55:13 +0530

    Add files via upload

---
+++ RedHeadDuck.java
@@ -0,0 +1,12 @@
+public class RedHeadDuck extends Duck {
+    
+    public RedHeadDuck() {
+        flyBehaviour = new Fly();
+        swimBehavior = new Swim();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Red Head Duck.");
+    }
+}
