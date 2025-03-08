bb44715d8a5549042189271743354af3ee5ebb8a
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-03-08 23:57:14 +0530

    Add files via upload

---
+++ Duck.java
@@ -0,0 +1,19 @@
+public abstract class Duck {
+    protected FlyBehaviour flyBehaviour;
+    protected SwimBehavior swimBehavior;
+
+    public Duck() {}
+
+    public void performFly() {
+        flyBehaviour.fly();
+    }
+
+    public void performSwim() {
+        swimBehavior.swim();
+    }
+
+    public abstract void display();
+}
+
+
+
